c4c5a18a272cf4aef3d3ebb514698d43ea95ac07
wintermute28 <a28c@yandex.ru>
2024-08-11 16:40:23 +0300

    style fixes

--- src/components/FilterBlock/FilterBlock.jsx
+++ src/components/FilterBlock/FilterBlock.jsx
@@ -1,26 +1,25 @@
-import React, { useContext } from 'react';
-import './FilterBlock.css'
-import { CoinsContext } from '../../context/coinsContext';
-import useFilterCoins from '../../helpers/hooks/useFilterCoins';
-const FilterBlock = ({setCoins}) => {
-
+import React, { useContext } from "react";
+import "./FilterBlock.css";
+import { CoinsContext } from "../../context/coinsContext";
+import useFilterCoins from "../../helpers/hooks/useFilterCoins";
+const FilterBlock = ({ setCoins }) => {
   const coinsContext = useContext(CoinsContext);
-  const {coins} = coinsContext;
+  const { coins } = coinsContext;
 
-  const {setValue, value} = useFilterCoins(setCoins, coins);
+  const { setValue, value } = useFilterCoins(setCoins, coins);
 
   return (
-    <div className='filter-block'>
-      <p className='filter-descr'>Find a coin:</p>
+    <div className="filter-block">
+      <p className="filter-descr">Find a coin:</p>
       <input
-
         type="text"
-        className='input'
-        placeholder='bitcoin'
+        className="input"
+        placeholder="bitcoin"
         value={value}
-        onChange={(e) => setValue(e.target.value)} />
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default React.memo(FilterBlock);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import './Header.css'
+import "./Header.css";
 
-const name = 'CRYPTO'
+const name = "CRYPTO";
 
 const Header = () => {
   return (
-    <header className='header'>{name}</header>
-  )
-}
+    <header className="header">
+      {/* {name} */}
+      <div className="sign-wrap-1">
+        <span className="sign_word">{name}</span>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
